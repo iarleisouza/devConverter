df7d4124340d2e36539a86cf1d6bd4cd5c5c58f8
iarleisouza <65626531+iarleisouza@users.noreply.github.com>
2023-10-12 22:35:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('form')
+form.addEventListener('submit', handleSubmit)
+
+const inputValue = document.getElementById('value-real')
+const selectedCurrency = document.getElementById('currency')
+const result = document.getElementById('result')
+let valueConverted = 0
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!inputValue.value || inputValue.value < 0) {
+        alert('Informe um valor correto!')
+        return
+    } else if(!selectedCurrency.value) {
+        alert('Escolha uma moeda!')
+        return
+    }
+    converter()
+}
+
+function converter() {
+    if(selectedCurrency.value === 'eur'){
+     valueConverted = inputValue.value / 6.22
+     result.innerHTML = valueFormatter('pt-br', 'EUR')
+        animateResult()
+    } else if(selectedCurrency.value === 'dol') {
+        valueConverted = inputValue.value / 5.37
+        result.innerHTML = valueFormatter('pt-br', 'EUR')
+    }
+    inputValue.value = ''
+    selectedCurrency.value = ''
+
+    animateResult()
+}
+
+function valueFormatter(locale, currency) {
+   const value = valueConverted.toLocaleString(`${locale}`, { style: 'currency', currency: `${currency}`})
+   return `<span>🤑 </span> ${value} <span>🤑 </span>`
+
+}
+
+function animateResult() {
+    return result.animate([
+        { transform: 'translateY(-150px)'},
+        { transform: 'translateY(0px)'},
+         ], { duration: 500})
+}
